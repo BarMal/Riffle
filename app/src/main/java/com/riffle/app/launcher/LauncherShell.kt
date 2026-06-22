d5e451fd728ad43765c218f2f534dae118d641c8
BarMal <barnabymalaj@gmail.com>
2026-06-22 15:28:21 +0100

    Apply safe drawing insets

--- app/src/main/java/com/riffle/app/launcher/LauncherShell.kt
+++ app/src/main/java/com/riffle/app/launcher/LauncherShell.kt
@@ -6,9 +6,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -100,6 +103,7 @@ private fun DefaultHomePrompt(onAction: (LauncherShellAction) -> Unit) {
         modifier =
             Modifier
                 .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.safeDrawing)
                 .padding(32.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
