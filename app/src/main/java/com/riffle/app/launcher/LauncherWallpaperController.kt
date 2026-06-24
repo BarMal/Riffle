96d8f6ec91d7a45c0097a3942b1e183b797549d5
BarMal <BarMal@users.noreply.github.com>
2026-06-24 09:12:13 +0100

    Make system wallpaper bars transparent (#186)

--- app/src/main/java/com/riffle/app/launcher/LauncherWallpaperController.kt
+++ app/src/main/java/com/riffle/app/launcher/LauncherWallpaperController.kt
@@ -2,8 +2,10 @@ package com.riffle.app.launcher
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.view.Window
 import android.view.WindowManager
+import androidx.core.view.WindowCompat
 
 interface LauncherWallpaperController {
     fun showSystemWallpaper()
@@ -14,6 +16,13 @@ class AndroidLauncherWallpaperController(
 ) : LauncherWallpaperController {
     override fun showSystemWallpaper() {
         window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isStatusBarContrastEnforced = false
+            window.isNavigationBarContrastEnforced = false
+        }
         window.setFlags(
             WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER,
             WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER,
