7756b37bc10e22e217b594fe43328bef9136a282
BarMal <BarMal@users.noreply.github.com>
2026-07-01 10:50:39 +0100

    Separate widget provider refresh from app refresh (#303)

--- app/src/main/java/com/riffle/app/launcher/LauncherShellRefreshScope.kt
+++ app/src/main/java/com/riffle/app/launcher/LauncherShellRefreshScope.kt
@@ -5,8 +5,13 @@ import kotlinx.coroutines.Job
 enum class LauncherShellRefreshScope {
     INSTALLED_APPS,
     NOTIFICATIONS,
+    WIDGET_PROVIDERS,
 }
 
 fun LauncherShellViewModel.refreshNotifications(): Job {
     return refreshInstalledApps(scope = LauncherShellRefreshScope.NOTIFICATIONS)
+}
+
+fun LauncherShellViewModel.refreshWidgetProviders(): Job {
+    return refreshInstalledApps(scope = LauncherShellRefreshScope.WIDGET_PROVIDERS)
 }
