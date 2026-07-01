49efe204d4229f86f809fdf46365a39567182cf5
BarMal <barnabymalaj@gmail.com>
2026-07-01 17:32:39 +0100

    Use edge-to-edge system bar API

--- app/src/main/java/com/riffle/app/launcher/LauncherWallpaperController.kt
+++ app/src/main/java/com/riffle/app/launcher/LauncherWallpaperController.kt
@@ -2,10 +2,8 @@ package com.riffle.app.launcher
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.os.Build
 import android.view.Window
 import android.view.WindowManager
-import androidx.core.view.WindowCompat
 
 interface LauncherWallpaperController {
     fun showSystemWallpaper()
@@ -16,13 +14,6 @@ class AndroidLauncherWallpaperController(
 ) : LauncherWallpaperController {
     override fun showSystemWallpaper() {
         window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        window.statusBarColor = Color.TRANSPARENT
-        window.navigationBarColor = Color.TRANSPARENT
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            window.isStatusBarContrastEnforced = false
-            window.isNavigationBarContrastEnforced = false
-        }
         window.setFlags(
             WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER,
             WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER,
